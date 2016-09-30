bb016017eb42a68ece69047b378628b5e2188a5f
Laura Pomeroy <laura.pomeroy@spsmail.cuny.edu>
2016-09-30 00:42:26 +0000

    Warmup 6 Task 1

---
+++ task_01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A kust names ELEMENTS and a list named OPERATION_ORDER"""
+
+
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
+            'Carbon')
+
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division',
+                    'Addition', 'Subtraction']
